26b494d9857441b6a02a3bbda27a66c7459b0389
dafral <dafral1996@gmail.com>
2018-09-18 10:14:11 +0200

    Session 1

---
+++ Session 1 - Sockets UDP/Session 1 - Sockets UDP - Client/main.cpp
@@ -0,0 +1,55 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#define port 8000
+
+#include "Windows.h"
+#include "WinSock2.h"
+#include "Ws2tcpip.h"
+
+bool InitializeLibs()
+{
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR)
+	{
+		// Log and handle error
+		return false;
+	}
+
+	return true;
+
+}
+
+bool CleanupLibs()
+{
+	int iResult = WSACleanup();
+	if (iResult != NO_ERROR)
+	{
+		//log and handle error
+		return false;
+	}
+
+	return true;
+
+}
+
+int main(int argc, char **argv)
+{
+	SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
+
+
+	struct sockaddr_in bindAddr;
+
+	bindAddr.sin_family = AF_INET;
+	bindAddr.sin_port = htons(port);
+	
+	//Destination IP address
+	struct sockaddr_in remoteAddr;
+	remoteAddr.sin_family = AF_INET; 
+	remoteAddr.sin_port = htons(port); 
+	const char *remoteAddrStr = "127.0.0.1"; 
+	inet_pton(AF_INET, remoteAddrStr, &remoteAddr.sin_addr);
+
+
+	closesocket(s);
+}
---
+++ Session 1 - Sockets UDP/Session 1 - Sockets UDP - Server/main.cpp
@@ -0,0 +1,57 @@
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include "Windows.h"
+#include "WinSock2.h"
+#include "Ws2tcpip.h"
+
+bool InitializeLibs()
+{
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR)
+	{
+		// Log and handle error
+		return false;
+	}
+
+	return true;
+
+}
+
+bool CleanupLibs()
+{
+	int iResult = WSACleanup();
+	if (iResult != NO_ERROR)
+	{
+		//log and handle error
+		return false;
+	}
+
+	return true;
+
+}
+
+int main(int argc, char **argv)
+{
+	SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
+
+	struct sockaddr_in bindAddr;
+
+	bindAddr.sin_family = AF_INET;
+	bindAddr.sin_port = htons(bindAddr.sin_port);
+
+	//IP address for bind()
+	bindAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	//Bind
+	int enable = 1;
+	int res = setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const char*)&enable, sizeof(int));
+	if (res == SOCKET_ERROR) {
+		// Log and handle error
+	}
+
+	res = bind(s, (const struct sockaddr *)&bindAddr, sizeof(bindAddr));
+
+	closesocket(s);
+}
